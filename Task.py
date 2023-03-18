e2b8c12d12b97f334a20e839607d717cdf1e96ef
AndreyLogvinovich <Hitri1ksh@gmail.com>
2023-03-18 14:03:15 +0300

    Добивили решение к первому уловию

--- Task.py
+++ Task.py
@@ -4,3 +4,16 @@
 что задано положительное целое число).
 Протестируем функцию на файле «article.txt» со следующим содержимым:
 """
+
+def read_last(lines, file):
+    if lines > 0:
+        with open(file, encoding='utf-8') as text:
+            file_lines = text.readlines()[-lines:]
+        for line in file_lines:
+            print(line.strip())
+        else:
+            print('Количество строк может быть только целым положительным')
+
+# Тесты 
+read_last(5, 'article.txt')
+read_last(-5, 'article.txt')
