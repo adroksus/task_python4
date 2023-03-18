68ef80d04c689d42c03949a8daf3f7cd76643fe8
AndreyLogvinovich <Hitri1ksh@gmail.com>
2023-03-18 14:08:11 +0300

    last commit

--- Task.py
+++ Task.py
@@ -17,3 +17,19 @@ def read_last(lines, file):
 # Тесты 
 read_last(5, 'article.txt')
 read_last(-5, 'article.txt')
+
+"""
+Требуется реализовать функцию longest_words(file), которая записывает в файл result.txt слово,
+имеющее максимальную длину (или список слов, если таковых несколько).
+"""
+def longest_words(file):
+    with open(file, encoding='utf-8') as text:
+        words = text.read().split()
+        max_length = len(max(words, key=len))
+        sought_words = [word for word in words if len(word) == max_length]
+        if len(sought_words) == 1:
+            return sought_words[0]
+        return sought_words
+ 
+ 
+print(longest_words('article.txt'))
